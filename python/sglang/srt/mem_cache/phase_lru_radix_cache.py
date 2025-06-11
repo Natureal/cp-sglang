1ef8bb1ff2b0ee1bc77fba1e4d67328c3227fb66
Natureal <naturechenpeng@gmail.com>
2025-06-11 20:46:10 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -174,6 +174,7 @@ class PhaseLRURadixCache(BasePrefixCache):
         self.distinct_element = set()
         self.evicted_ts = {}
         self.inv_count = 0
+        logger.info(f"TreeNode.counter = {TreeNode.counter}, deleted_node_count = {self.deleted_node_count}")
         logger.info(f"start a new phase, current_node_count: {TreeNode.counter - self.deleted_node_count}, decrease phase_err_param from {self.phase_err_param} to {int(math.sqrt(self.phase_err_param))}")
 
         self.phase_err_param = int(math.sqrt(self.phase_err_param))
