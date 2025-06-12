58b31c60d05405c52648fc179fc38e4905758750
Natureal <naturechenpeng@gmail.com>
2025-06-12 17:02:27 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -331,8 +331,8 @@ class PhaseLRURadixCache(BasePrefixCache):
                 rank = self.sorted_list.bisect_left(self.pred_evicted_ts[address])
                 self.rank_sum += rank
                 logger.info(f"rank: {rank}, sum of inversions: {self.rank_sum}")
-                self.lru_budget = 0
-                #self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
+                #self.lru_budget = 0
+                self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
                 #self.lru_budget = 100000000
                 logger.info(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
 
