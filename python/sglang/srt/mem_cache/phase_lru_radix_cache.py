3119569b56ea6aa63e7607fc6ec5539913ac122c
Natureal <naturechenpeng@gmail.com>
2025-06-12 20:29:19 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -137,7 +137,7 @@ class PhaseLRURadixCache(BasePrefixCache):
         self.lru_evicted_ts = {}
         self.inv_count = 0
         self.sorted_list = SortedList()
-        self.lru_budget = 0
+        self.lru_budget = 10000000
 
         # global-level
         self.deleted_node_count = 0
@@ -193,7 +193,7 @@ class PhaseLRURadixCache(BasePrefixCache):
 
         self.phase_err_param = int(math.sqrt(self.phase_err_param))
         #self.sorted_list = SortedList()
-        self.lru_budget = 0
+        self.lru_budget = 10000000
         
 
     def match_prefix(self, key: List[int], **kwargs) -> Tuple[torch.Tensor, int]:
@@ -335,8 +335,8 @@ class PhaseLRURadixCache(BasePrefixCache):
                 self.rank_sum += rank
                 logger.info(f"rank: {rank}, sum of inversions: {self.rank_sum}")
                 #self.lru_budget = 0
-                self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
-                #self.lru_budget = 100000000
+                #self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
+                self.lru_budget = 100000000
                 logger.info(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
 
         if address in self.lru_evicted_ts:
