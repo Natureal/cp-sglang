7fd687e32d9b4ee7385f7284e549823c17cae7fa
Natureal <naturechenpeng@gmail.com>
2025-06-13 13:30:18 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -422,7 +422,7 @@ class PhaseLRURadixCache(BasePrefixCache):
         token_ids = req.fill_ids
         self.dump_req_keys.append(token_ids)
         if len(self.dump_req_keys) >= 100:
-            file_name = 'dump_req_keys_' + str(self.dump_file_count)
+            file_name = 'dump_req_keys_' + str(self.dump_file_count) + '.pkl'
             self.dump_file_count += 1
             with open(file_name, 'rb') as f:
                 pickle.dump(self.dump_req_keys, f)
