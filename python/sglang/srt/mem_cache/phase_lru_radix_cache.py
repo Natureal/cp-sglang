d4ca9db5fc5d4ce2b41ec881a0027e6240a24367
Natureal <naturechenpeng@gmail.com>
2025-06-18 17:25:48 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -881,13 +881,13 @@ if __name__ == "__main__":
     tree = PhaseLRURadixCache(None, None, page_size=1, disable=False)
     #tree.set_algo_type("phaselru") #popu
     #tree.set_algo_type("lru")
-    tree.set_algo_type("phaselru")
-    #tree.set_algo_type("belady")
+    #tree.set_algo_type("phaselru")
+    tree.set_algo_type("belady")
 
-    sync_send_req_set = load_multiturn_data(tokenizer)
-    data_type = "multiturn"
-    #sync_send_req_set = load_stress_test_data(tokenizer)
-    #data_type = "stress"
+    #sync_send_req_set = load_multiturn_data(tokenizer)
+    #data_type = "multiturn"
+    sync_send_req_set = load_stress_test_data(tokenizer)
+    data_type = "stress"
 
     #if tree.algo_type == "belady":
     current_ts = 0
