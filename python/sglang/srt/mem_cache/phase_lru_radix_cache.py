c0dd3e1d929f7ceccc14bf6302e61164c3a80e43
Natureal <naturechenpeng@gmail.com>
2025-06-18 11:06:30 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -371,19 +371,20 @@ class PhaseLRURadixCache(BasePrefixCache):
                 self.pred_evict_count += 1
                 self.pred_rank_sum += rank
                 logger.info(f"rank: {rank}, sum of inversions: {self.pred_rank_sum}, pred avg inv = {self.pred_rank_sum / self.pred_evict_count}")
-                #self.lru_budget = 0
-                self.phase_err_param = min(self.phase_err_param * 10, 100000000)
-                self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
+                self.lru_budget = 0
+                #self.phase_err_param = min(self.phase_err_param * 10, 100000000)
+                #self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
                 #self.lru_budget = self.phase_cache_k
                 print(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
                 
                 logger.info(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
 
         if address in self.lru_evicted:
-            rank = len(self.U)
-            self.lru_evict_count += 1
-            self.lru_rank_sum += rank
-            logger.info(f"lru_evicted, rank: {rank}, sum of inversions: {self.lru_rank_sum}, lru avg inv = {self.lru_rank_sum / self.lru_evict_count}")
+            if len(self.U) > 0:
+                rank = len(self.U)
+                self.lru_evict_count += 1
+                self.lru_rank_sum += rank
+                logger.info(f"lru_evicted, rank: {rank}, sum of inversions: {self.lru_rank_sum}, lru avg inv = {self.lru_rank_sum / self.lru_evict_count}")
 
     def set_algo_type(self, algo_type):
         if self.algo_type != algo_type:
