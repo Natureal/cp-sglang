0041c04fd4909359b25d393683de646d356bc885
Natureal <naturechenpeng@gmail.com>
2025-06-18 16:03:13 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -555,7 +555,7 @@ class PhaseLRURadixCache(BasePrefixCache):
                 node.pred_valid = 1
 
         for i in range(len(node_to_pred)):
-            print(f"node hash = {node.hash_value}, pred = {node.pred}, truth = {NRT[node.hash_value][0] if len(NRT[node.hash_value]) > 0 else 10000000}")
+            print(f"node key hash = {hash(tuple(node.key))}, hash_value = {node.hash_value}, pred = {node.pred}, truth = {NRT[node.hash_value][0] if len(NRT[node.hash_value]) > 0 else 10000000}")
 
     def _evict_by_lru(self, num_tokens: int, based_on_budget: bool):
         leaves = self._collect_leaves()
