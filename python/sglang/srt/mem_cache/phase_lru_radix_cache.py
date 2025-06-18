c0cf0e86eb53ad42c6d871d05f4f4857be7292e9
Natureal <naturechenpeng@gmail.com>
2025-06-18 14:29:37 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -375,9 +375,9 @@ class PhaseLRURadixCache(BasePrefixCache):
                 self.pred_evict_count += 1
                 self.pred_rank_sum += rank
                 logger.info(f"rank: {rank}, sum of inversions: {self.pred_rank_sum}, pred avg inv = {self.pred_rank_sum / self.pred_evict_count}")
-                self.lru_budget = 0
-                #self.phase_err_param = min(self.phase_err_param * 2, 100000000)
-                #self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
+                #self.lru_budget = 0
+                self.phase_err_param = min(self.phase_err_param * 2, 100000000)
+                self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
                 #self.lru_budget = self.phase_cache_k
                 print(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
                 
