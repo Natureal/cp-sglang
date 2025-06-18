bd68ac55142b6fa1cb676fbe616899254ad35665
Natureal <naturechenpeng@gmail.com>
2025-06-18 16:31:48 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -380,7 +380,7 @@ class PhaseLRURadixCache(BasePrefixCache):
             self.phase_err_param = min(self.phase_err_param * 2, 100000000)
             self.lru_budget = min(self.lru_budget + self.phase_err_param, 100000000)
             #self.lru_budget = self.phase_cache_k
-            print(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
+            print(f"current_ts = {self.current_ts}, reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
             
             logger.info(f"reset lru_budget = {self.lru_budget}, phase_err_param = {self.phase_err_param}")
 
