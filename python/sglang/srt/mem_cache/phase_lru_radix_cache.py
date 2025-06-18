0c2ce04ec0d21fdc2fdbecb924035e7417a51cf5
Natureal <naturechenpeng@gmail.com>
2025-06-18 16:50:45 +0800

    update

--- python/sglang/srt/mem_cache/phase_lru_radix_cache.py
+++ python/sglang/srt/mem_cache/phase_lru_radix_cache.py
@@ -899,6 +899,8 @@ if __name__ == "__main__":
                 NRT[prefix_hash] = deque()
             NRT[prefix_hash].append(current_ts)
 
+    print(f"debug: {NRT[-4232634994979945749]}")
+
     total_size = 60000
     req_count = 0
     current_size = 0
