897de42b86f80cf51c232263a6ae2cdbdeb22cb4
Natureal <naturechenpeng@gmail.com>
2025-06-20 15:35:38 +0800

    update

--- benchmark/hicache/bench_process_qwen_bailian_data.py
+++ benchmark/hicache/bench_process_qwen_bailian_data.py
@@ -4,12 +4,18 @@ import json
 
 
 if __name__ == "__main__":
-    data_list = []
+    session = {}
     with open('qwen_traceA_blksz_16.jsonl', 'r', encoding='utf-8') as f:
         for line in f:
             if line.strip():  # 跳过空行
-                data_list.append(json.loads(line))
+                chat_id = line['chat_id']
+                if chat_id not in session:
+                    session[chat_id] = [line]
+                else:
+                    session[chat_id].append(line)
 
-    print(f"line1 = {str(data_list[0])}")
-    print(f"line2 = {str(data_list[1])}")
-    print(f"line3 = {str(data_list[2])}")
+    print(f"session len: {len(session)}")
+    for chat in session:
+        print(f" ------------- one session starts --------------- ")
+        for req in chat:
+            print(f"req: {str(req)}")
