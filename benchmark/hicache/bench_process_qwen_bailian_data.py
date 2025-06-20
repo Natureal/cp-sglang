bad141aa35a6cc0ba3289d208a6fdc2ada049610
Natureal <naturechenpeng@gmail.com>
2025-06-20 15:29:19 +0800

    update

--- benchmark/hicache/bench_process_qwen_bailian_data.py
+++ benchmark/hicache/bench_process_qwen_bailian_data.py
@@ -4,7 +4,10 @@ import json
 
 
 if __name__ == "__main__":
+    data_list = []
     with open('qwen_traceA_blksz_16.jsonl', 'r', encoding='utf-8') as f:
-        data = json.load(f)
+        for line in f:
+            if line.strip():  # 跳过空行
+                data_list.append(json.loads(line))
 
-    print(f"type of f = {type(data)}")
+    print(f"first line = {str(data_list[0])}")
