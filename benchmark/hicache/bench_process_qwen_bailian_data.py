66cb27de49b07c3debb7850732df1207d83d88c7
Natureal <naturechenpeng@gmail.com>
2025-06-20 15:37:30 +0800

    update

--- benchmark/hicache/bench_process_qwen_bailian_data.py
+++ benchmark/hicache/bench_process_qwen_bailian_data.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     with open('qwen_traceA_blksz_16.jsonl', 'r', encoding='utf-8') as f:
         for line in f:
             if line.strip():  # 跳过空行
-                print(f"type of line: {type(line)}")
-                chat_id = line['chat_id']
+                chat_id = json.loads(line['chat_id'])
                 if chat_id not in session:
                     session[chat_id] = [line]
                 else:
