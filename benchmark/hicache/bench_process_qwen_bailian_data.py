ec962623b01fefd5af2f90ca02f29da6c556a6ac
Natureal <naturechenpeng@gmail.com>
2025-06-20 17:12:33 +0800

    update

--- benchmark/hicache/bench_process_qwen_bailian_data.py
+++ benchmark/hicache/bench_process_qwen_bailian_data.py
@@ -1,8 +1,5 @@
 import json
 
-
-
-
 if __name__ == "__main__":
     chat_dict = {}
     parent = {}
@@ -10,31 +7,32 @@ if __name__ == "__main__":
         for line in f:
             if line.strip():  # 跳过空行
                 item = json.loads(line)
-                chat_id = item['chat_id']
-                chat_dict[chat_id] = item
-                parent_chat_id = item['parent_chat_id']
+                print(f"{str(item)}")
+                #chat_id = item['chat_id']
+                #chat_dict[chat_id] = item
+                #parent_chat_id = item['parent_chat_id']
 
-    link_count = 0
-    for chat_id, item in chat_dict.items():
-        parent_chat_id = item['parent_chat_id']
-        if parent_chat_id != -1:
-            link_count += 1
-            chat_dict[parent_chat_id]['child_id'] = chat_id
-            print(f"link, from {parent_chat_id} to {chat_id}")
+    # link_count = 0
+    # for chat_id, item in chat_dict.items():
+    #     parent_chat_id = item['parent_chat_id']
+    #     if parent_chat_id != -1:
+    #         link_count += 1
+    #         chat_dict[parent_chat_id]['child_id'] = chat_id
+    #         print(f"link, from {parent_chat_id} to {chat_id}")
 
-    print(f"link count = {link_count}")
+    # print(f"link count = {link_count}")
 
-    print(f"chat_dict len: {len(chat_dict)}")
-    for chat_id, item in chat_dict.items():
-        req_count = 0
-        if item['parent_chat_id'] != -1:
-            continue
-        print(f" ------------- one session starts --------------- ")
-        while True:
-            req_count += 1
-            print(f"req: {str(item)}")
-            if 'child_id' in item:
-                item = chat_dict[item['child_id']]
-            else:
-                break
-        print(f" ------------- one session ends, req count = {req_count} --------------- ")
+    # print(f"chat_dict len: {len(chat_dict)}")
+    # for chat_id, item in chat_dict.items():
+    #     req_count = 0
+    #     if item['parent_chat_id'] != -1:
+    #         continue
+    #     print(f" ------------- one session starts --------------- ")
+    #     while True:
+    #         req_count += 1
+    #         print(f"req: {str(item)}")
+    #         if 'child_id' in item:
+    #             item = chat_dict[item['child_id']]
+    #         else:
+    #             break
+    #     print(f" ------------- one session ends, req count = {req_count} --------------- ")
